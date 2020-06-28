fd3e585405eb8564bf26ace198cdf41ca76daa93
RameezIsrarVagrant <rameezisrarulhaq@gmail.com>
2020-06-28 15:42:37 +0000

    Add the items seeder class that will populate the items table

---
+++ database/seeds/ItemSeeder.php
@@ -0,0 +1,80 @@
+<?php
+
+use App\Models\Item;
+use Illuminate\Database\Seeder;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the item seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $item = new Item;
+        $item->type_id = "1";  //assuming the type is already been created via db seed command
+        $item->name = "TRULY ITALIAN";
+        $item->price = "10.99";
+        $item->description = "Pepperoni Pizza With Thin Crust";
+        $item->image = "/img/pizza/pizza1.png";
+        $item->save();
+
+        $item = new Item;
+        $item->type_id = "1";  //assuming the type is already been created via db seed command
+        $item->name = "WILDERNESS BLESS";
+        $item->price = "15.99";
+        $item->description = "Pepperoni Pizza With Thin CrustHam & Cheese With Vegetables";
+        $item->image = "/img/pizza/pizza2.png";
+        $item->save();
+
+        $item = new Item;
+        $item->type_id = "1";  //assuming the type is already been created via db seed command
+        $item->name = "GREEN DELIGHT";
+        $item->price = "18.99";
+        $item->description = "Vegetarian Pizza With Thin Crust";
+        $item->image = "/img/pizza/pizza3.png";
+        $item->save();
+
+        $item = new Item;
+        $item->type_id = "1";  //assuming the type is already been created via db seed command
+        $item->name = "FEELING CHEESY";
+        $item->price = "20.99";
+        $item->description = "Pepperoni Pizza With Four Cheeses";
+        $item->image = "/img/pizza/pizza4.png";
+        $item->save();
+
+        $item = new Item;
+        $item->type_id = "1";  //assuming the type is already been created via db seed command
+        $item->name = "Hot & Spicy";
+        $item->price = "22.99";
+        $item->description = "Spicy chicken, onions, green peppers & jalapenos";
+        $item->image = "/img/pizza/pizza5.png";
+        $item->save();
+
+        $item = new Item;
+        $item->type_id = "1";  //assuming the type is already been created via db seed command
+        $item->name = "Golden Delight";
+        $item->price = "24.99";
+        $item->description = "Unique combo of marinated chicken & pineapples";
+        $item->image = "/img/pizza/pizza6.png";
+        $item->save();
+
+        $item = new Item;
+        $item->type_id = "1";  //assuming the type is already been created via db seed command
+        $item->name = "Legend - Ranch";
+        $item->price = "28.99";
+        $item->description = "Blend of grilled chicken & sliced cheese,";
+        $item->image = "/img/pizza/pizza7.png";
+        $item->save();
+
+        $item = new Item;
+        $item->type_id = "1";  //assuming the type is already been created via db seed command
+        $item->name = "Tex-Mex";
+        $item->price = "30.99";
+        $item->description = "Blend of grilled chicken";
+        $item->image = "/img/pizza/pizza8.png";
+        $item->save();
+
+    }
+}
