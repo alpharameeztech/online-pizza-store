8839dfe19fa074b4871f75c1354058dc9bde1db4
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-06-28 20:37:48 +0500

    Rename the type migration file

--- database/migrations/2020_06_28_153557_create_types_table.php
+++ database/migrations/2020_06_28_153557_create_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTypesTableTable extends Migration
+class CreateTypesTable extends Migration
 {
     /**
      * Run the migrations.
