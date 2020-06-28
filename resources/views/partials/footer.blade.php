a5e697dcb98a3f307018e4c9a180a07ca81f442e
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-06-28 21:11:58 +0500

    Add view partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,23 @@
+<!-- Footer Start -->
+<footer class="ct-footer footer-dark">
+  <!-- Top Footer -->
+  <!-- Footer Bottom -->
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="footer-copyright">
+        <p> Made with
+          <span class="material-icons">
+            favorite
+            </span>
+          by
+          <strong>
+              Rameez Israr
+          </strong>
+          (upcoming member of the Innoscripta). </p>
+        <a href="#" class="back-to-top">Back to top <i class="fas fa-chevron-up"></i> </a>
+      </div>
+    </div>
+  </div>
+
+</footer>
+<!-- Footer End -->
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,38 @@
+<!-- Header Start -->
+<header class="main-header header-1 header-absolute">
+
+  <div class="top-header">
+    <div class="container">
+      <div class="top-header-inner">
+
+        <div class="top-header-contacts">
+          <ul class="top-header-nav">
+            <li> <a class="p-0" href="tel:+123456789"><i class="fas fa-phone mr-2"></i> +123 456 789</a> </li>
+          </ul>
+        </div>
+
+        <ul class="top-header-nav header-cta">
+          <li> <a >Order Online</a> </li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <nav class="navbar">
+      <!-- Logo -->
+      <a class="navbar-brand" href="/"> <img src="/img/logo.png" alt="logo"> </a>
+      <!-- Menu -->
+
+      <cart-icon-component></cart-icon-component>
+
+    </nav>
+  </div>
+
+</header>
+<!-- Header End -->
+
+<!-- Cart Sidebar Start -->
+  <cart-component></cart-component>
+<!-- Cart Sidebar End -->
