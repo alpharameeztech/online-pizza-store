5f24477703b8b7e118bf83a32f958e1376d752f7
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-06-28 21:17:48 +0500

    Home route should list all menu items for the guest user

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ItemController@index');
 
-Route::get('/home', 'HomeController@index')->name('home');
